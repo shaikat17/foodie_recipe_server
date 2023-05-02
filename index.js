27880f77983152e922d490554ff161828871271b
shaikat17 <shaikatpal56@gmail.com>
2023-05-02 17:11:01 +0600

    chefs json data added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const chefs = require('./public/chefs.json')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs)
 })
 
 app.listen(port, () => {
