0355a0c25e02324400732bba8b0eeca9b6928f63
shaikat17 <shaikatpal56@gmail.com>
2023-05-03 02:37:33 +0600

    single chef get by id

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const express = require('express')
-const chefs = require('./data/chefs.json')
-const app = express()
-const port = 3000
-const cors = require("cors")
+const express = require("express");
+const chefs = require("./data/chefs.json");
+const recipes = require("./data/recipes.json");
+const app = express();
+const port = 3000;
+const cors = require("cors");
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/chefs', (req, res) => {
-  res.send(chefs)
-})
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChef = chefs.find((chef) => chef.id == id);
+  // console.log(singleChef);
+  res.send(singleChef)
+});
+
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
